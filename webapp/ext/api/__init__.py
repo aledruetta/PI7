feb5b0590c1b647e65a2ca7ba7e48025de56f628
Alejandro Druetta <aledruetta@gmail.com>
2021-06-05 11:08:45 -0300

    Updates

--- webapp/ext/api/__init__.py
+++ webapp/ext/api/__init__.py
@@ -7,8 +7,8 @@ api = Api()
 
 api.add_resource(ApiUserAll, "/api/user")
 api.add_resource(ApiUser, "/api/user/<string:user_id>")
-api.add_resource(ApiThingAll, "/api/topic")
-api.add_resource(ApiThing, "/api/topic/<string:topic_id>")
+api.add_resource(ApiThingAll, "/api/thing")
+api.add_resource(ApiThing, "/api/thing/<string:thing_id>")
 
 
 def init_app(app):
